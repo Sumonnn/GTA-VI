da4fb818561a3d9132dc4c7cbf5c84377e0c1e00
Sumonnn <sumonkhan1081@gmail.com>
2025-07-05 15:58:40 +0530

    app setup with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>Welcome to the world of GTA!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
